3b7c89a08a1c0b2df0b2c3b56c4f3d5f96ba5069
RusackDmiry <rusack.dmitr.free@gmail.com>
2018-01-13 19:58:14 +0300

    Add files via upload

---
+++ ClassLibrary1/ClassLibrary1/Operation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    public class Operation
+    {
+        public static double Sum(double obj1, double obj2)
+        {
+            return obj1 + obj2;
+        }
+        public static double Razn(double obj1, double obj2)
+        {
+            return obj1 + obj2;
+        }
+        public static double Del(double obj1, double obj2)
+        {
+            return obj1 + obj2;
+        }
+        public static double Umn(double obj1, double obj2)
+        {
+            return obj1 + obj2;
+        }
+    }
+}
---
+++ ClassLibrary1/ClassLibrary1/TestOperation.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ClassLibrary1
+{
+    class TestOperation
+    {
+        [TestFixture]
+        public class OperationTest
+        {
+            
+            private double resultOperation;
+            
+            [Test]
+            public void TestSum()
+            {
+                resultOperation = Operation.Sum(1, 2);
+                Assert.AreEqual(3, resultOperation);
+
+                resultOperation = Operation.Sum(-1, 2);
+                Assert.AreEqual(1, resultOperation);
+
+                resultOperation = Operation.Sum(0, 2);
+                Assert.AreEqual(2, resultOperation);
+
+                resultOperation = Operation.Sum(0.1, 2);
+                Assert.AreEqual(2.1, resultOperation);
+
+                resultOperation = Operation.Sum(-10, 2);
+                Assert.AreEqual(-8, resultOperation);
+            }
+
+            [Test]
+            public void TestRazn()
+            {
+                resultOperation = Operation.Razn(1, 1);
+                Assert.AreEqual(0, resultOperation);
+
+                resultOperation = Operation.Razn(0, 0);
+                Assert.AreEqual(0, resultOperation);
+
+                resultOperation = Operation.Razn(5, 1);
+                Assert.AreEqual(4, resultOperation);
+
+                resultOperation = Operation.Razn(1, 5);
+                Assert.AreEqual(-4, resultOperation);
+
+                resultOperation = Operation.Razn(1.1, 0.1);
+                Assert.AreEqual(1, resultOperation);
+            }
+
+            [Test]
+            public void TestDel()
+            {
+                resultOperation = Operation.Del(1, 1);
+                Assert.AreEqual(1, resultOperation);
+
+                resultOperation = Operation.Del(2, 1);
+                Assert.AreEqual(2, resultOperation);
+
+                resultOperation = Operation.Del(6, 3);
+                Assert.AreEqual(2, resultOperation);
+
+                resultOperation = Operation.Del(-2, 1);
+                Assert.AreEqual(-2, resultOperation);
+
+                resultOperation = Operation.Del(0, 1);
+                Assert.AreEqual(0, resultOperation);
+            }
+
+            [Test]
+            public void TestUmn()
+            {
+                resultOperation = Operation.Umn(2, 2);
+                Assert.AreEqual(4, resultOperation);
+
+                resultOperation = Operation.Umn(6, -2);
+                Assert.AreEqual(-3, resultOperation);
+
+                resultOperation = Operation.Umn(0, 2);
+                Assert.AreEqual(0, resultOperation);
+
+                resultOperation = Operation.Umn(10, 2);
+                Assert.AreEqual(20, resultOperation);
+
+                resultOperation = Operation.Umn(2, 0.5);
+                Assert.AreEqual(1, resultOperation);
+            }
+        }
+
+    }
+}
